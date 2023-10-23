b56fc59e4620a37b52cc4e92b1064acc561f62ff
alpererdin <alpererdin636@hotmail.com>
2023-10-23 11:49:23 +0300

    ++

---
+++ Assets/Scripts/Runtime/Data/UnityObject/CD_Level.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Runtime.Data.UnityObject
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class CD_Level : ScriptableObject
+    {
+        
+    }
+}
