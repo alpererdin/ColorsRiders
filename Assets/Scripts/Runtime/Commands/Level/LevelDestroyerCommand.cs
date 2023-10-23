b265601ad073ba0c85eb67e8ef4ce48eeffa1527
alpererdin <alpererdin636@hotmail.com>
2023-10-23 11:49:30 +0300

    ++

--- Assets/Scripts/Runtime/Commands/Level/LevelDestroyerCommand.cs
+++ Assets/Scripts/Runtime/Commands/Level/LevelDestroyerCommand.cs
@@ -1,4 +1,4 @@
-using Runtime.Interfaces;
+/*using Runtime.Interfaces;
 using Runtime.Managers;
 using UnityEngine;
 
@@ -16,6 +16,26 @@ namespace Runtime.Commands.Level
         public void Execute()
         {
             Object.Destroy(_levelManager.levelHolder.transform.GetChild(0).gameObject);
+        }
+    }
+}*/
+using UnityEngine;
+
+namespace Runtime.Commands.Level
+{
+    public class LevelDestroyerCommand
+    {
+        private Transform _levelHolder;
+
+        internal LevelDestroyerCommand(Transform levelHolder)
+        {
+            _levelHolder = levelHolder;
+        }
+
+        internal void Execute()
+        {
+            if (_levelHolder.transform.childCount <= 0) return;
+            Object.Destroy(_levelHolder.transform.GetChild(0).gameObject);
         }
     }
 }
