a8c35f0130dbac97b10505eb1c1e2c2ff66e3357
alpererdin <alpererdin636@hotmail.com>
2023-11-03 23:42:55 +0300

    ++

---
+++ Assets/Scripts/Runtime/tt/RoadChecker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Runtime.Managers;
+using UnityEngine;
+
+public class RoadChecker : MonoBehaviour
+{
+    #region variables
+
+
+    public DroneAreaManager road1, road2;
+   
+    #endregion
+    
+    
+    
+}
