2c101effc63f2acad13f2b61a3ce8a4e4eafa731
alpererdin <alpererdin636@hotmail.com>
2023-11-16 08:03:08 +0300

    ++

--- Assets/Scripts/Runtime/Commands/Stack/ResetStackCommand.cs
+++ Assets/Scripts/Runtime/Commands/Stack/ResetStackCommand.cs
@@ -1,7 +1,18 @@
+ 
+using System.Collections.Generic;
+using Runtime.Data.ValueObject;
+using Runtime.Managers;
+using UnityEngine;
+
 namespace Runtime.Commands.Stack
 {
     public class ResetStackCommand
     {
-        
+        private StackManager _stackManager;
+        private List<GameObject> _collectableStack;
+        private StackData _data;
+
+
     }
+
 }
