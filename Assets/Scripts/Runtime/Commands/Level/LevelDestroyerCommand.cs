9d18f55ffa810fc4d178d7bd8e71d5f4ff9087e9
alpererdin <alpererdin636@hotmail.com>
2023-12-10 16:03:24 +0300

    ++

--- Assets/Scripts/Runtime/Commands/Level/LevelDestroyerCommand.cs
+++ Assets/Scripts/Runtime/Commands/Level/LevelDestroyerCommand.cs
@@ -1,24 +1,4 @@
-/*using Runtime.Interfaces;
-using Runtime.Managers;
-using UnityEngine;
-
-namespace Runtime.Commands.Level
-{
-    public class LevelDestroyerCommand : ICommand
-    {
-        private readonly LevelManager _levelManager;
-
-        public LevelDestroyerCommand(LevelManager levelManager)
-        {
-            _levelManager = levelManager;
-        }
-
-        public void Execute()
-        {
-            Object.Destroy(_levelManager.levelHolder.transform.GetChild(0).gameObject);
-        }
-    }
-}*/
+ 
 using UnityEngine;
 
 namespace Runtime.Commands.Level
